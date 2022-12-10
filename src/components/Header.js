166a0932832d27283738678cae02814354d0d9b1
Ari Jaya <arijaya130@gmail.com>
2022-12-10 19:23:14 +0800

    adding SignOut feature

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import styled from 'styled-components';
 import { useDispatch, useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
@@ -6,7 +7,8 @@ import {
     selectUserName, 
     selectUserPhoto, 
     setUserLoginDetails,
-} from '../features/user/userSlice'
+    setSignOutState,
+} from '../features/user/userSlice';
 
 const Header = (props) => {
     const dispatch = useDispatch();
@@ -14,7 +16,17 @@ const Header = (props) => {
     const userName = useSelector(selectUserName);
     const userPhoto = useSelector(selectUserPhoto);
 
+    useEffect(() => {
+        auth.onAuthStateChanged(async (user) => {
+            if(user) {
+                setUser(user)
+                history.push('./home')
+            }
+        });
+    }, [userName]);
+
     const handleAuth = () => {
+        if (!userName) {
         auth
             .signInWithPopup(provider)
             .then((result) => {
@@ -23,6 +35,15 @@ const Header = (props) => {
         .catch((error) => {
             alert(error.message);
         });
+    } else if (userName) {
+        auth
+            .signOut()
+            .then(() => {
+                dispatch(setSignOutState())
+                history.push('/')
+            })
+            .catch((err) => alert(err.message));
+        }
     };
 
     const setUser = (user) => {
@@ -72,7 +93,12 @@ const Header = (props) => {
                             <span>SERIES</span>
                         </a>
                     </NavMenu>
-                    <UserImg src={userPhoto} alt={userName} />
+                    <SignOut>
+                        <UserImg src={userPhoto} alt={userName} />
+                        <DropDown>
+                            <span onClick={handleAuth}>Sign out</span>
+                        </DropDown>
+                    </SignOut>
                 </>
             )}
         </Nav>
@@ -191,6 +217,44 @@ const Login =styled.a`
 
 const UserImg = styled.img`
     height: 100%;
+`;
+
+const DropDown = styled.div`
+    position: absolute;
+    top: 48px;
+    right: 0px;
+    background: rgb(19, 19, 19);
+    border: 1px solid rgba(151, 151, 151, 0.34);
+    border-radius: 4px;
+    box-shadow: rgb(0 0 0 / 50%) 0px 0px 18px 0px;
+    padding: 10px;
+    font-size: 14px;
+    letter-spacing: 3px;
+    width: 100px;
+    opacity: 0;
+`;
+
+const SignOut = styled.div`
+    position: relative;
+    height: 48px;
+    width: 48px;
+    display: flex;
+    cursor: pointer;
+    align-items: center;
+    justify-content: center;
+
+    ${UserImg} {
+        border-radius: 50%;
+        width: 100%;
+        height: 100%;
+    }
+
+    &:hover {
+        ${DropDown} {
+            opacity: 1;
+            transition-duration: 1s;
+        }
+    }
 `;
 
 export default Header
