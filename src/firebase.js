9c0b47270ff672d53f8764252dc7cb151f3f4c04
Ari Jaya <arijaya130@gmail.com>
2022-12-10 13:35:48 +0800

    complit page 1 feture

---
+++ src/firebase.js
@@ -0,0 +1,20 @@
+import firebase from 'firebase'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBAWqPk5kRhExQawlvzr3rVnHoSxGmRs3w",
+    authDomain: "ari-disneyclone-a1706.firebaseapp.com",
+    projectId: "ari-disneyclone-a1706",
+    storageBucket: "ari-disneyclone-a1706.appspot.com",
+    messagingSenderId: "472211741552",
+    appId: "1:472211741552:web:18ef3692a99d3cb1e40db5",
+    measurementId: "G-LR5JD7W15C"
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+const storage = firebase.storage();
+
+export { auth, provider, storage };
+export default db;
